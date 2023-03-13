c3a2184c861f4297c64f72fabb3d184a552e9d70
samanthacsik <samanthacsik@gmail.com>
2023-03-12 21:26:56 -0700

    cleaning script & data

--- data-wrangling/salaries.R
+++ data-wrangling/salaries.R
@@ -1,8 +1,11 @@
 # women in the workplace: https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-03-05
 # https://twitter.com/thomas_mock/status/1102576601760448512
+
 #..........................load packages.........................
 library(tidyverse)
-library(patchwork)
+library(cowplot)
+library(scales)
+library(ggtext)
 
 #..........................import data...........................
 jobs_gender <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-03-05/jobs_gender.csv")
@@ -25,114 +28,78 @@ jobs_gender_clean <- jobs_gender |>
 see_NAs <- jobs_gender_clean %>% 
   naniar::miss_var_summary() 
   
-# saveRDS(jobs_gender_clean, here::here("raw-data", "jobs.rds"))
+# saveRDS(jobs_gender_clean, here::here("raw-data", "all_jobs.rds"))
 
 #.......................pre-plot wrangling.......................
 
-
-######################## female 75% #########################
-
-# filter for year 2016 & occupations that are >=75% female ----
-female_75perc <- jobs_gender_clean |> 
-  filter(year == "2016") |> 
-  filter(percent_female >= 75) #|>
-  # slice_sample(n = 16) |> 
-
-# get 16 random jobs
-# set.seed(123)
-# random_occupations_75perc_male <- sample_n(female_75perc, 16)
-
-# select occupations that are >= 75% female ----
+# select occupations for plotting ----
 select_occupations_75perc_female <- c("Dancers and choreographers", "Librarians", "Elementary and middle school teachers",
-                        "Registered nurses", "Dietitians and nutritionists", "Occupational therapists",
-                        "Compensation, benefits, and job analysis specialists",
-                        "Secretaries and administrative assistants",
-                        "Medical assistants", "Hairdressers, hairstylists, and cosmetologists", "Veterinary assistants and laboratory animal caretakers", "Hosts and hostesses, restaurant, lounge, and coffee shop")
+                                      "Registered nurses", "Dietitians and nutritionists", "Occupational therapists",
+                                      "Secretaries and administrative assistants", "Medical assistants", "Hairdressers, hairstylists, and cosmetologists", 
+                                      "Veterinary assistants and laboratory animal caretakers")
 
-female_75perc_select_occupations <- female_75perc |> 
-  filter(occupation %in% select_occupations_75perc_female) |> 
-  # mutate(avg_salary = ((total_earnings_male + total_earnings_female)/2)) |> 
-  arrange(-avg_salary)
-
-######################## 45-55% female #########################
-
-# filter for year 2016 & occupations that are >=75% male ----
-female_45_55perc <- jobs_gender_clean |> 
-  filter(year == "2016") |> 
-  filter(percent_female >= 45 & percent_female <= 55) #|>
-# slice_sample(n = 16) |> 
-
-# get 16 random jobs
-# set.seed(123)
-# random_occupations_75perc_male <- sample_n(male_75perc, 16)
-
-# select occupations that are >= 75% female ----
 select_occupations_45_55perc_female <- c("Legislators", "Natural sciences managers", "Medical scientists", 
                                          "Postsecondary teachers", "Designers", "Bartenders", "Real estate brokers and sales agents",
-                                         "Pharmacists", "Credit analysts", "Animal trainers", "Insurance sales agents", 
-                                         "Editors")
+                                         "Pharmacists", "Insurance sales agents", "Editors")
 
-female_45_55perc_select_occupations <- female_45_55perc |> 
-  filter(occupation %in% select_occupations_45_55perc_female) |> 
-  # mutate(avg_salary = ((total_earnings_male + total_earnings_female)/2)) |> 
-  arrange(-avg_salary)
+select_occupations_75perc_male <- c("Chief executives", "Software developers, applications and systems software", "Mathematicians", 
+                                    "Chemical engineers", "Civil engineers", "Conservation scientists and foresters",
+                                    "Police and sheriff's patrol officers", "Chefs and head cooks",
+                                    "Construction and building inspectors", "Aircraft pilots and flight engineers")
 
-######################## male 75% #########################
+select_occupations <- c(select_occupations_75perc_female, select_occupations_45_55perc_female, select_occupations_75perc_male)
 
-# filter for year 2016 & occupations that are >=75% male ----
-male_75perc <- jobs_gender_clean |> 
-  filter(year == "2016") |> 
-  filter(percent_male >= 75) #|>
-# slice_sample(n = 16) |> 
+jobs_for_plotting <- jobs_gender_clean |> 
+  filter(year == 2016) |> 
+  mutate(perc_group = case_when(
+    percent_female >= 75 ~ "f75",
+    percent_female >= 45 & percent_female <= 55 ~ "f50",
+    percent_male >= 75 ~"m75"
+  )) |> 
+  mutate(perc_group = fct_relevel(perc_group, "f75", "f50", "m75")) |> 
+  drop_na(perc_group) |> 
+  filter(occupation %in% select_occupations)
 
-# get 16 random jobs
-# set.seed(123)
-# random_occupations_75perc_male <- sample_n(male_75perc, 16)
-
-# select occupations that are >= 75% female ----
-select_occupations_75perc_male <- c("Chief executives",
-                               "Software developers, applications and systems software", "Mathematicians", "Chemical engineers", "Civil engineers", "Conservation scientists and foresters",
-                               "Police and sheriff's patrol officers", "Chefs and head cooks",
-                               "Construction and building inspectors", "Precision instrument and equipment repairers", "Electricians",
-                               "Aircraft pilots and flight engineers")
-
-male_75perc_select_occupations <- male_75perc |> 
-  filter(occupation %in% select_occupations_75perc_male) |> 
-  # mutate(avg_salary = ((total_earnings_male + total_earnings_female)/2)) |> 
-  arrange(-avg_salary)
+# saveRDS(jobs_for_plotting, here::here("clean-data", "select_jobs.rds"))
 
 #..............................plot..............................
-female75_plot <- ggplot(female_75perc_select_occupations) +
+earnings_plot <- ggplot(jobs_for_plotting) +
+  
+  # create dumbbells
   geom_segment(aes(x = reorder(occupation, avg_salary), xend = occupation, y = total_earnings_female, yend = total_earnings_male)) +
   geom_point(aes(x = occupation, y = total_earnings_male), color = "#CD93D8", size = 2.5) +
-  geom_point(aes(x = occupation, y = total_earnings_female), color = "#6A1E99", fill = "#6A1E99", size = 2.5, shape = 23) +
-  coord_flip() + 
-  theme(
-    legend.position = "none"
-  )
-
-female75_plot
-
-female45_55_plot <- ggplot(female_45_55perc_select_occupations) +
-  geom_segment(aes(x = reorder(occupation, avg_salary), xend = occupation, y = total_earnings_female, yend = total_earnings_male)) +
-  geom_point(aes(x = occupation, y = total_earnings_male), color = "#CD93D8", size = 2.5) +
-  geom_point(aes(x = occupation, y = total_earnings_female), color = "#6A1E99", fill = "#6A1E99", size = 2.5, shape = 23) +
+  geom_point(aes(x = occupation, y = total_earnings_female), color = "#6A1E99", size = 2.5) +
+  
+  # flip axes & facet wrap by group
   coord_flip() +
+  facet_wrap(~perc_group, nrow = 3, scale = "free_y") +
+  
+  # axis breaks & $ labels
+  scale_y_continuous(breaks = c(25000, 50000, 75000, 100000, 125000),
+                     labels = scales::label_dollar(scale = 0.001, suffix = "k")) + 
+  
+  # title & subtitle (add styling)
+  labs(title = "**Earnings by Occupation and Sex**",
+       subtitle = "Median earnings of full-time <span style='color:#CD93D8'>**male**</span> versus <span style='color:#6A1E99'>**female**</span> workers by occupation in 2016",
+       caption = "<span style='color:#919092'>*Data courtesy of TidyTuesday (March 5, 2019)*</span>") +
+  
+  # theme
+  theme_minimal() +
   theme(
-    legend.position = "none"
+    
+    # facet panels
+    strip.background = element_blank(),
+    strip.text.x = element_blank(),
+    
+    # title & subtitle
+    plot.title.position = "plot", # left-align plot title with left-edge of plot, not y-axis (see: https://github.com/tidyverse/ggplot2/issues/3252)
+    plot.title = element_markdown(), # # enable markdown text (ggtext)
+    plot.subtitle = element_markdown(margin = margin(t = 0, r = 0, b = 10, l = 0)), # enable markdown text (ggtext) 
+    plot.caption = element_markdown(margin = margin(t = 15, r = 0, b = 0, l = 0)), # enable markdown text (ggtext)  
+    
+    # axes
+    axis.title = element_blank(),
+    
   )
-
-female45_55_plot
-
-male75_plot <- ggplot(male_75perc_select_occupations) +
-  geom_segment(aes(x = reorder(occupation, avg_salary), xend = occupation, y = total_earnings_female, yend = total_earnings_male)) +
-  geom_point(aes(x = occupation, y = total_earnings_male), color = "#CD93D8", size = 2.5) +
-  geom_point(aes(x = occupation, y = total_earnings_female), color = "#6A1E99", fill = "#6A1E99", size = 2.5, shape = 23) +
-  coord_flip() +
-  theme(
-    legend.position = "none"
-  )
-
-male75_plot
-
-female75_plot + female45_55_plot + male75_plot + plot_layout(nrow = 3, byrow = FALSE)
+  
+earnings_plot
